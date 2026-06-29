237bab04d8d00114e29a08e4cc5bb9e76a8d7c0f
Gaurav yadav <163104508+Gauravvydv07@users.noreply.github.com>
2026-06-29 09:44:15 +0000

    Add 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445537
+let accountEmail = "gaurav133@gamil.com"
+var accountCity = "kolapur"
+accountPassword = "1212122"
+
+// accountId = 12121212 not allowed 
+
+accountEmail = "ga@.com"
+accountCity = "sangli"
+accountPassword = "90909090"
+
+console.log(accountId);
+
+console.table ([accountId, accountEmail, accountCity, accountPassword])
