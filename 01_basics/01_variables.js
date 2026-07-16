3def5686d864819444ba180fbde3149e5d920535
Gauravvydv07 <gauravyadav1483@gmail.com>
2026-07-16 15:01:41 +0530

    update variables practice

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,12 +3,13 @@ let accountEmail = "gaurav133@gamil.com"
 var accountCity = "kolapur"
 accountPassword = "1212122"
 
-// accountId = 12121212 not allowed 
+// accountId = 12121212 not allowed const cant change the value
 
-accountEmail = "ga@.com"
-accountCity = "sangli"
+accountEmail = "ga@.com"// we cant redeclare (let) here...it gone show some error if we redclear thire 
+accountCity = "sangli" // (var)can be redeclare.
 accountPassword = "90909090"
 
 console.log(accountId);
+console.log(accountCity);
 
-console.table ([accountId, accountEmail, accountCity, accountPassword])
+// console.table ([accountId, accountEmail, accountCity, accountPassword])
