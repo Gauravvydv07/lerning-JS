eaf45ef33c121652d777c4bdc3fbd97e1131c920
Gauravvydv07 <gauravyadav1483@gmail.com>
2026-07-16 16:30:34 +0530

    add dataypes practice

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//"use strict";// this is (satment)for treat all js code as newer version for engine usally dont have to write this now new js already have this.
+
+//alert(3 + 3)// this (stament) we derectanly cant oprat becuse in console js is runing on brower and vs code it is running on node because of this it is only can oprat for console for vs code thire is diffrent syntax for alert statment.(we are using nodejs, not brower).
+
+let name = "Gaurav"//string => ""
+let age = 24 //number = 2 to power 53 this is maximum renge we can have no as a datatype after that new datatype come that is *(bigint)= to large no above the no we use bigint datatype
+let isLoggedIn = true //boolean : in the form of true and false
+// null => standalone value.(mens empty value not 0)
+// undefined => value is not assgined.
+// symbol => uniqeness.....
+// THIS ALL ARE PRIMITIVE DATATYPES it is a just catoerization
+
+//object
+
+console.log(typeof undefined);// this is just tell which type of data is.
+console.log(typeof null); object//when we serch (typeof null) it shows the object is a type of data of null ..
+// null is a object
